b59355408bed821512777275b00d678baf155d07
eason920 <eason920@gmail.com>
2019-04-25 01:19:09 +0800

    cocolong

--- cocolong/js/coding_by_lambert.js
+++ cocolong/js/coding_by_lambert.js
@@ -1,12 +1,24 @@
 $(function(){
+		//alert("got");
+	//$(".lsview_m_bg").parallax("50%",0.05);
+	// $(".lsview_m_sofa").parallax("50%",0.075);
+	// $(".lsview_m_snow1").parallax("50%",0.5);
+	// $(".lsview_m_snow2").parallax("50%",0.15);
+	////
 	$(".lsview_pc_bg").parallax("50%",0.075);
 	$(".lsview_pc_sofa").parallax("50%",0.1);
 	$(".lsview_pc_snow1").parallax("50%",0.3);
 	$(".lsview_pc_snow2").parallax("50%",0.15);
 
-	_view_m_height=$(".lsview_m_area").height();
-	$(".lsview_m_bg").css({height:_view_m_height});
+	////
 
+	//$(window).resize(function(){
+		_view_m_height=$(".lsview_m_area").height();
+		$(".lsview_m_bg").css({height:_view_m_height});
+		// $(".lsview_m_sofa").css({height:_view_m_height});
+		// $(".lsview_m_snow1").css({height:_view_m_height});
+		// $(".lsview_m_snow2").css({height:_view_m_height});
+	//});
 	$(".lsfooter_mbtn").click(function(){
 		$('html,body').animate({ scrollTop: 0 }, 'slow');
         return false; 
