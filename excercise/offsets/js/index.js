ec336ed3035986b574570ec2ffe7edb87158dbf2
eason-wfe <easonshyu@worldfamilyenglish.com>
2019-04-25 17:57:07 +0800

    offset-1

--- excercise/offsets/js/index.js
+++ excercise/offsets/js/index.js
@@ -3,7 +3,7 @@
 	// isDown 是用來記錄是否按住不放
 	var $doc = $(document), 
 		$selected = null, 
-		_zIndex = 10;
+		_zindex = 10;
 
 	$('.dragDiv1, .dragDiv2 h3').mousedown(function(event){
 		event.preventDefault();
@@ -11,16 +11,21 @@
 		var $this = $(this);
 
 		if($this.hasClass('dragDiv')){
-			$selected = $this;
+			$selected = $this; // for dragDiv1
 		}else{
-			$selected = $this.parents('.dragDiv');
+			$selected = $this.parents('.dragDiv');// for dragDiv2
 		}
 
 		var _offset = $selected.offset(), 
 			_x = _offset.left - event.pageX, 
 			_y = _offset.top - event.pageY;
+		console.log('offsets() is '+_offset);
+		console.log('_offset.left is'+_offset.left);
+		console.log('event.pageX is '+event.pageX);
+		console.log('_offset.top is '+_offset.top);
+		console.log('event.pageY is '+event.pageY);
 
-		$selected.css('z-index', _zIndex++).addClass('draggable');
+		$selected.css('z-index', _zindex++).addClass('draggable');
 
 		$doc.on('mousemove.abgne', function(event){
 			$selected.css({
