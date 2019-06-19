277c493968451eee696ba8879d33735da47e541b
eason920 <eason920@gmail.com>
2019-06-20 00:18:09 +0800

    0620-1

--- public/js/filter.js
+++ public/js/filter.js
@@ -1,33 +1,33 @@
-$(function(){
+const render = function (html, data) {
+	let temp = '';
+	for (let i in data) {
+		temp += html;
+		for (let key in data[i]) {
+			const orgStr = `{{${key}}}`;
+			const newStr = data[i][key];
+			temp = temp.replace(orgStr, newStr);
+		}
+	}
+	return temp;
+};
+
+const show = function (data) {
+	$.get('temp.html', function (html) {
+		$('#list').html(render(html, data));
+	})
+}
+
+$(function () {
 	let json = [];
-	$.getJSON('data/filter.json', function(data){
+	$.getJSON('data/filter.json', function (data) {
 		json = data;
 		show(json);
 	});
 
-	const render = function(html, data){
-		let temp = '';
-		for(let i in data){
-			temp += html;
-			for(let key in data[i]){
-				const orgStr = `{{${key}}}`;
-				const newStr = data[i][key];
-				temp = temp.replace(orgStr, newStr);
-			}
-		}
-		return temp;
-	};
-
-	const show = function(data){
-		$.get('temp.html', function(html){
-			$('#list').html(render(html, data));
-		})
-	}
-
-	$('#search').on('keyup', function(){
-	let val = $(this).val().toLowerCase();
+	$('#search').on('keyup', function () {
+		let val = $(this).val().toLowerCase();
 		show(
-			json.filter(function(item){
+			json.filter(function (item) {
 				return item.name.toLowerCase().indexOf(val) >= 0;
 			})
 		);
--- public/js/sort.js
+++ public/js/sort.js
@@ -1,80 +1,81 @@
-﻿$(function() {
-    var json;
-    function show(data) {
-        var html = "";
-        for (var i = 0; i < data.length; i++) {
-            html += "<tr>";
-            html += "<td>" + data[i]["date"] + "</td>";
-            html += "<td>" + data[i]["className"] + "</td>";
-            html += "<td>" + data[i]["peoples"] + "</td>";
-            html += "<td>" + data[i]["tw"] + "</td>";
-            html += "</tr>";
-        }
-        $("#content").html(html);
-    }
-    $.getJSON("data/sort.json", function(data) {
-        json = data;
-        show(json);
-    });
-    $(".bordered th").click(function() {
-        var $this = $(this);
-        $this.siblings().removeClass();
-        if ($this.hasClass("down")) {
-            $this.attr("class", "up");
-        } else {
-            $this.attr("class", "down");
-        }
-        var key = $this.data("key");
-        var direction = $this.attr("class");
-        sort(key, direction);
-        show(json);
-    });
-    function sort(key, direction) {
-        if( key !== 'tw' ){
-            json.sort(function (a, b) {
-                if (direction == "down") {
-                    if (a[key] > b[key]) {
-                        return 1;
-                    } else {
-                        return -1;
-                    }
-                } else {
-                    if (a[key] < b[key]) {
-                        return 1;
-                    } else {
-                        return -1;
-                    }
-                }
-            });
-        }else{
-            json.sort(function (a, b) {
-                let aa = customSort(a[key]);
-                let bb = customSort(b[key]);
-                if (direction == "down") {
-                    if (aa > bb) {
-                        return 1;
-                    } else {
-                        return -1;
-                    }
-                } else {
-                    if (aa < bb) {
-                        return 1;
-                    } else {
-                        return -1;
-                    }
-                }
-            });
-        }
-        
-    }
-    function customSort(input) {
-        if (typeof input == "string") {
-            input = input.replace("一","1");
-            input = input.replace("二","2");
-            input = input.replace("三","3");
-            input = input.replace("四","4");
-            input = input.replace("五","5");
-        }
-        return input;
-    }
+const show = function (data) {
+	let html = '';
+	for (let i in data) {
+		html += '<tr>';
+		for (let key in data[i]) {
+			html += `<td>${data[i][key]}</td>`
+		}
+		html += '</tr>';
+	}
+	$('#content').html(html);
+}
+
+const customSort = function (input) {
+	input = input.replace('一', '1');
+	input = input.replace('二', '2');
+	input = input.replace('三', '3');
+	input = input.replace('四', '4');
+	input = input.replace('五', '5');
+	return input;
+}
+
+const sort = function (data, direct, key) {
+	show(
+		data.sort(function(nex, pre){
+			if(key !== 'tw'){
+				if (direct === 'down') {
+					if (nex[key] > pre[key]) {
+						return 1;
+					} else {
+						return -1;
+					}
+				} else {
+					if (nex[key] < pre[key]) {
+						return 1;
+					} else {
+						return -1;
+					}
+				}
+			}else{
+				let nn = customSort(nex[key]);
+				let pp = customSort(pre[key]);
+				if (direct === 'down') {
+					if (nn > pp) {
+						return 1;
+					} else {
+						return -1;
+					}
+				} else {
+					if (nn < pp) {
+						return 1;
+					} else {
+						return -1;
+					}
+				}
+			}
+			return data;
+		})
+	);
+};
+
+$(function () {
+	let json = [];
+	$.getJSON('data/sort.json', function (data) {
+		json = data;
+		show(json);
+	});
+
+	$('th').on('click', function () {
+		const $this = $(this);
+		if ($this.hasClass('down')) {
+			$('th').removeClass();
+			$this.addClass('up');
+		} else {
+			$('th').removeClass();
+			$this.addClass('down');
+		}
+		const direct = $this.attr('class');
+		const key = $this.data('key');
+		sort(json, direct, key);
+	});
 });
