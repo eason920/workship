50b325023d6c090526e7207bbbad76ae22658bdd
eason920 <eason920@gmail.com>
2019-06-20 14:56:42 +0800

    0620-2

--- public/js/page.js
+++ public/js/page.js
@@ -1,58 +1,85 @@
-﻿$(function () {
-	var json;
-	var pagerCount = 5; //每頁顯示幾筆
-	var currentIndex = 1; //目前在第幾頁
-	function show(data) {
-		var $tbody = $("<tbody>");
-		for (var i = 0; i < data.length; i++) {
-			$tbody.append(
-				$("<tr>").append(
-					$("<td>").append(
-						$("<a>").attr("href", data[i].url).text(data[i].title)
-					)
-				)
-			);
-		}
-		$("#content").html($tbody.html());
-		$('#content a').click(function () {
-			return false;
-		});
+﻿const show = function (data) {
+	let $tbody = $('<tbody>');
+	for (i = 0; i < data.length; i++) {
+		$tbody.append(
+			$('<tr>').append(
+				$('<td>').append(
+					$('<a>').text(data[i].title).attr('href', data[i].url)
+				)));
+	};
+	$('#content').html($tbody.html());
+}
+
+const page = function (data, currentIndex, perIndexPage) {
+	const totalPage = data.length - 1;
+	const firstPage = (currentIndex - 1) * perIndexPage //0 , 5, 20;
+	const latestPage = firstPage + perIndexPage - 1 //4, 9, 24
+	const totalIndex = Math.ceil(data.length / perIndexPage);
+	//
+	if (currentIndex > totalIndex) {
+		currentIndex = totalIndex;
 	}
-	$.getJSON("data/page.json", function (data) {
+	if (latestPage >= totalPage) {
+		lastPage = totalPage;
+	}
+	let newData = [];
+	for (i = 0; i <= totalPage; i++) {
+		if (i >= firstPage && i <= latestPage) {
+			newData.push(data[i]);
+		}
+	}
+
+	// page nation
+	let html = '';
+	for (i = 1; i <= totalIndex; i++) {
+		html += '<li>'
+		if (currentIndex !== i) {
+			html += `<a href='#' data-index='${i}'>${i}`
+		} else {
+			html += `<a href='#' data-index='${i}' class='active'>${i}`
+		}
+		html += '</a></li>'
+	}
+	$('#pageNation').html(html);
+
+	return newData;
+}
+
+$(function () {
+	let json = [];
+	let currentIndex = 1;
+	const perIndexPage = 5;
+	let totalIndex = '';
+
+	$.getJSON('data/page.json', function (data) {
 		json = data;
-		show(pager(json, pagerCount, currentIndex));
+		totalIndex = Math.ceil(data.length / perIndexPage);
+		show(page(json, currentIndex, perIndexPage));
 	});
-	function pager(data, count, index) {
-		return data.filter(function (a) {
-			var i = data.indexOf(a); //目前資料的索引位置
-			return i >= count * (index - 1) && i < count * index;
-		});
-	}
-	$("#first").click(function () {
+
+	$('#pageNation').on('click', 'a', function (e) {
+		e.preventDefault();
+		currentIndex = $(this).data('index');
+		show(page(json, currentIndex, perIndexPage));
+	});
+
+	$('#prev').on('click', function () {
+		currentIndex <= 1 ? currentIndex = 1 : currentIndex--;
+		show(page(json, currentIndex, perIndexPage));
+	});
+
+	$('#next').on('click', function () {
+		currentIndex >= totalIndex ? currentIndex = totalIndex : currentIndex++;
+		show(page(json, currentIndex, perIndexPage));
+	});
+
+	$('#first').on('click', function () {
 		currentIndex = 1;
-		show(pager(json, pagerCount, currentIndex));
+		show(page(json, currentIndex, perIndexPage));
 	});
-	$("#prev").click(function () {
-		currentIndex--;
-		if (currentIndex < 1) {
-			currentIndex = 1;
-			alert("沒有上一頁了");
-		} else {
-			show(pager(json, pagerCount, currentIndex));
-		}
-	});
-	$("#next").click(function () {
-		currentIndex++;
-		var maxPagerIndex = Math.ceil(json.length / pagerCount);
-		if (currentIndex > maxPagerIndex) {
-			currentIndex = maxPagerIndex;
-			alert("沒有下一頁了");
-		} else {
-			show(pager(json, pagerCount, currentIndex));
-		}
-	});
-	$("#last").click(function () {
-		currentIndex = Math.ceil(json.length / pagerCount);
-		show(pager(json, pagerCount, currentIndex));
+
+	$('#last').on('click', function () {
+		currentIndex = totalIndex;
+		show(page(json, currentIndex, perIndexPage));
 	});
 });
