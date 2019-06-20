dc36acd7eb77dc568f8ddee5c5b011fda575d634
eason920 <eason920@gmail.com>
2019-06-20 17:58:17 +0800

    0620-3

--- public/js/page.js
+++ public/js/page.js
@@ -11,25 +11,8 @@
 }
 
 const page = function (data, currentIndex, perIndexPage) {
-	const totalPage = data.length - 1;
-	const firstPage = (currentIndex - 1) * perIndexPage //0 , 5, 20;
-	const latestPage = firstPage + perIndexPage - 1 //4, 9, 24
+	// PAGE NATION
 	const totalIndex = Math.ceil(data.length / perIndexPage);
-	//
-	if (currentIndex > totalIndex) {
-		currentIndex = totalIndex;
-	}
-	if (latestPage >= totalPage) {
-		lastPage = totalPage;
-	}
-	let newData = [];
-	for (i = 0; i <= totalPage; i++) {
-		if (i >= firstPage && i <= latestPage) {
-			newData.push(data[i]);
-		}
-	}
-
-	// page nation
 	let html = '';
 	for (i = 1; i <= totalIndex; i++) {
 		html += '<li>'
@@ -42,25 +25,33 @@ const page = function (data, currentIndex, perIndexPage) {
 	}
 	$('#pageNation').html(html);
 
-	return newData;
+	// LIST
+	const firstPage = (currentIndex - 1) * perIndexPage
+	const latestPage = currentIndex * perIndexPage
+	return data.filter(function(a){
+		let i = data.indexOf(a);
+		return i >= firstPage && i < latestPage;
+	});
 }
 
 $(function () {
 	let json = [];
 	let currentIndex = 1;
-	const perIndexPage = 5;
-	let totalIndex = '';
-
+	const perIndexPage = 6;
+	let totalIndex;
 	$.getJSON('data/page.json', function (data) {
 		json = data;
-		totalIndex = Math.ceil(data.length / perIndexPage);
+		totalIndex = Math.ceil(json.length / perIndexPage);
 		show(page(json, currentIndex, perIndexPage));
 	});
 
 	$('#pageNation').on('click', 'a', function (e) {
 		e.preventDefault();
-		currentIndex = $(this).data('index');
-		show(page(json, currentIndex, perIndexPage));
+		const $this = $(this);
+		if( !$this.hasClass('active')){
+			currentIndex = $this.data('index');
+			show(page(json, currentIndex, perIndexPage));			
+		}
 	});
 
 	$('#prev').on('click', function () {
