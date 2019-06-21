07e3ee02b286320f2c8ceb838fa96d3673af2f54
eason920 <eason920@gmail.com>
2019-06-21 14:22:24 +0800

    page.js fix

--- public/js/page.js
+++ public/js/page.js
@@ -1,76 +1,76 @@
 ﻿const show = function (data) {
-	let $tbody = $('<tbody>');
+	let html = ''
 	for (i = 0; i < data.length; i++) {
-		$tbody.append(
-			$('<tr>').append(
-				$('<td>').append(
-					$('<a>').text(data[i].title).attr('href', data[i].url)
-				)));
-	};
-	$('#content').html($tbody.html());
+		html += `<tr><td>`
+		html += `<a href='${data[i].url}'>${data[i].title}`
+		html += `</a></td></tr>`
+	}
+	$('#content').html(html);
+
 }
 
-const page = function (data, currentIndex, perIndexPage) {
+const page = function (data, per, current) {
 	// PAGE NATION
-	const totalIndex = Math.ceil(data.length / perIndexPage);
+	const maxIndex = Math.ceil(data.length / per);
 	let html = '';
-	for (i = 1; i <= totalIndex; i++) {
-		html += '<li>'
-		if (currentIndex !== i) {
-			html += `<a href='#' data-index='${i}'>${i}`
-		} else {
-			html += `<a href='#' data-index='${i}' class='active'>${i}`
-		}
-		html += '</a></li>'
+	for (i = 1; i <= maxIndex; i++) {
+		html += `<li>`
+		i !== current ? html += `<a href='#'>${i}` : html += `<a href='#' class='active'>${i}`
+		html += `</a></li>`
 	}
 	$('#pageNation').html(html);
 
 	// LIST
-	const firstPage = (currentIndex - 1) * perIndexPage
-	const latestPage = currentIndex * perIndexPage
-	return data.filter(function(a){
-		let i = data.indexOf(a);
-		return i >= firstPage && i < latestPage;
+	const first = per * (current - 1);
+	const last = per * current;
+	return data.filter(function (item) {
+		let i = data.indexOf(item);
+		return i >= first && i < last;
 	});
 }
 
 $(function () {
 	let json = [];
-	let currentIndex = 1;
+	let currentIndex = 3;
+	let maxIndex = 0;
 	const perIndexPage = 6;
-	let totalIndex;
 	$.getJSON('data/page.json', function (data) {
 		json = data;
-		totalIndex = Math.ceil(json.length / perIndexPage);
-		show(page(json, currentIndex, perIndexPage));
+		maxIndex = Math.ceil(json.length / perIndexPage);
+		show(page(json, perIndexPage, currentIndex));
 	});
 
 	$('#pageNation').on('click', 'a', function (e) {
 		e.preventDefault();
-		const $this = $(this);
-		if( !$this.hasClass('active')){
-			currentIndex = $this.data('index');
-			show(page(json, currentIndex, perIndexPage));			
+		$this = $(this);
+		if (!$this.hasClass('active')) {
+			currentIndex = Number($this.text());
+			show(page(json, perIndexPage, currentIndex));
 		}
-	});
-
-	$('#prev').on('click', function () {
-		currentIndex <= 1 ? currentIndex = 1 : currentIndex--;
-		show(page(json, currentIndex, perIndexPage));
-	});
-
-	$('#next').on('click', function () {
-		currentIndex >= totalIndex ? currentIndex = totalIndex : currentIndex++;
-		show(page(json, currentIndex, perIndexPage));
 	});
 
 	$('#first').on('click', function () {
 		currentIndex = 1;
-		show(page(json, currentIndex, perIndexPage));
+		show(page(json, perIndexPage, currentIndex));
 	});
 
 	$('#last').on('click', function () {
-		currentIndex = totalIndex;
-		show(page(json, currentIndex, perIndexPage));
+		currentIndex = maxIndex;
+		show(page(json, perIndexPage, currentIndex));
 	});
+
+	$('#prev').on('click', function () {
+		currentIndex > 1 ? currentIndex-- : currentIndex = 1;
+		show(page(json, perIndexPage, currentIndex));
+	});
+
+	$('#next').on('click', function () {
+		currentIndex < maxIndex ? currentIndex++ : currentIndex = maxIndex;
+		show(page(json, perIndexPage, currentIndex));
+	});
+
+	// DEBUG
+	$('#content').on('click', 'a', function (e) {
+		e.preventDefault();
+	})
 });
